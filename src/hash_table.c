a78effa85c16285abd98e6a27eeffb50c271aafb
Dipshan Adhikari <dipshanadh@gmail.com>
2023-05-13 13:31:50 +0545

    Hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "hash_table.h"
 
-static ht_item* ht_new_item(const char *key, const char *value) {
+static ht_item* ht_new_item(const char *k, const char *v) {
 	ht_item *i 	= malloc(sizeof(ht_item));
 
-	i->key 		= strdup(key);
-	i->value	= strdup(value);
+	i->key 		= strdup(k);
+	i->value	= strdup(v);
 
 	return i;
 }
@@ -38,4 +39,16 @@ void ht_del_hash_table(ht_hash_table *ht) {
 
 	free(ht->items);
 	free(ht);
+}
+
+static int ht_hash(const char *s, const int a, const int m) {
+	long hash = 0;
+	const int len_s = strlen(s);
+
+	for(int i = 0; i < len_s; i++) {
+		hash += (long)pow(a, len_s - (i + 1)) * s[i];
+		hash = hash % m;
+	}
+
+	return (int)hash;
 }
