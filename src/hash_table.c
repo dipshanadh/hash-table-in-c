e73d82e8f1d741152751711320bbce5711efe5ce
Dipshan Adhikari <dipshanadh@gmail.com>
2023-05-30 20:52:33 +0545

    Deletion

--- src/hash_table.c
+++ src/hash_table.c
@@ -7,6 +7,8 @@
 #define HT_PRIME_1 151
 #define HT_PRIME_2 163
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 static ht_item* ht_new_item(const char *k, const char *v) {
 	ht_item *i 	= malloc(sizeof(ht_item));
 
@@ -76,7 +78,7 @@ void ht_insert(ht_hash_table *ht, const char *key, const char *value) {
 		index		= ht_get_hash(item->key, ht->size, i);
 		curr_item 	= ht->items[index];
 		i++;
-	} while(curr_item != NULL);
+	} while(curr_item != NULL && curr_item != &HT_DELETED_ITEM);
 
 	ht->items[index] = item;
 	ht->count++;
@@ -96,7 +98,30 @@ char *ht_search(ht_hash_table *ht, const char *key) {
 			return item->value;
 
 		i++;
-	} while(item != NULL);
+	} while(item != NULL && item != &HT_DELETED_ITEM);
 
 	return NULL;
+}
+
+void ht_delete(ht_hash_table *ht, const char *key) {
+	ht_item *item;
+
+	int index;
+	int i = 0;
+
+	do {
+		index	= ht_get_hash(key, ht->size, i);
+		item	= ht->items[index];
+
+		if(item != &HT_DELETED_ITEM)
+			if(strcmp(item->key, key) == 0) {
+				ht_del_item(item);
+				ht->items[index] = &HT_DELETED_ITEM;
+			}
+
+
+		i++;
+	} while(item != NULL);
+
+	ht->count--;
 }
